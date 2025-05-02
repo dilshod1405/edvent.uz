98720de61305a0ef02b3a4538411c2b8516338ac
Dilshod <dilshod.normurodov1392@gmail.com>
2025-05-02 23:17:17 +0900

    updated logo

--- components/ui/logo.jsx
+++ components/ui/logo.jsx
@@ -1,11 +1,11 @@
 import Link from "next/link";
 import Image from "next/image";
-import logo from "@/public/images/logo.svg";
+import logo from "@/public/images/logos.png";
 
 export default function Logo() {
   return (
     <Link href="/" className="inline-flex shrink-0" aria-label="Cruip">
-      <Image src={logo} alt="Cruip Logo" width={48} height={48} />
+      <Image src={logo} alt="Cruip Logo" width={200} height={200} className="w-30 md:w-40 lg:w-60 xs:w-20 sm:w-20"/>
     </Link>
   );
 }
