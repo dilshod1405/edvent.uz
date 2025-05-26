dd2fc58b1aaed8314e2d7f415bbef68d0e356afa
Dilshod <dilshod.normurodov1392@gmail.com>
2025-05-26 22:49:22 +0900

    updated

--- app/(auth)/reset-password/page.jsx
+++ app/(auth)/reset-password/page.jsx
@@ -3,24 +3,27 @@ export const metadata = {
     description: "Page description",
   };
   
+import Footer from "@/components/ui/footer";
+import Header from "@/components/ui/header";
   import Link from "next/link";
   
   export default function ResetPassword() {
     return (
       <section>
-        <div className="mx-auto max-w-6xl px-4 sm:px-6">
+        <Header />
+        <div className="max-w-6xl px-4 mx-auto sm:px-6">
           <div className="py-12 md:py-20">
             {/* Section header */}
             <div className="pb-12 text-center">
               <h1 className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,var(--color-gray-200),var(--color-indigo-200),var(--color-gray-50),var(--color-indigo-300),var(--color-gray-200))] bg-[length:200%_auto] bg-clip-text font-nacelle text-3xl font-semibold text-transparent md:text-4xl">
-                Reset your password
+                Parolni tiklash
               </h1>
             </div>
             {/* Contact form */}
             <form className="mx-auto max-w-[400px]">
               <div>
                 <label
-                  className="mb-1 block text-sm font-medium text-indigo-200/65"
+                  className="block mb-1 text-sm font-medium text-indigo-200/65"
                   htmlFor="email"
                 >
                   Email
@@ -28,18 +31,20 @@ export const metadata = {
                 <input
                   id="email"
                   type="email"
-                  className="form-input w-full"
-                  placeholder="Your email"
+                  className="w-full form-input"
+                  placeholder="Email manzilingizni kiriting"
+                  required
                 />
               </div>
               <div className="mt-6">
                 <button className="btn w-full bg-linear-to-t from-indigo-600 to-indigo-500 bg-[length:100%_100%] bg-[bottom] text-white shadow-[inset_0px_1px_0px_0px_--theme(--color-white/.16)] hover:bg-[length:100%_150%]">
-                  Reset Password
+                  RYuborish
                 </button>
               </div>
             </form>
           </div>
         </div>
+        <Footer />
       </section>
     );
   }
