27bd70e2b1c327d0871ad142d05037de73e7edd7
Dilshod <dilshod.normurodov1392@gmail.com>
2025-05-31 22:12:13 +0900

    updated videoplayer

--- app/course/lesson/VideoPlayer.jsx
+++ app/course/lesson/VideoPlayer.jsx
@@ -4,7 +4,7 @@ export default function VideoPlayer({ title, otp, playbackInfo }) {
   const iframeSrc = `https://player.vdocipher.com/v2/?otp=${otp}&playbackInfo=${playbackInfo}`;
 
   return (
-    <div className="p-6 bg-gray-900 rounded-lg shadow-lg">
+    <div className="p-6">
       <h1 className="mb-4 text-3xl font-bold">{title}</h1>
       <iframe
         src={iframeSrc}
