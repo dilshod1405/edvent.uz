f7e42a9d998243df07e3953fe6e5d9c70e3964d2
Dilshod <dilshod.normurodov1392@gmail.com>
2025-06-01 12:43:10 +0900

    updated

--- app/course/lesson/VideoPlayer.jsx
+++ app/course/lesson/VideoPlayer.jsx
@@ -1,11 +1,11 @@
 'use client';
 
-export default function VideoPlayer({ title, otp, playbackInfo }) {
+export default function VideoPlayer({ title, otp, playbackInfo, key }) {
   const iframeSrc = `https://player.vdocipher.com/v2/?otp=${otp}&playbackInfo=${playbackInfo}`;
 
   return (
     <div className="p-6">
-      <h1 className="mb-4 text-3xl font-bold">{title}</h1>
+      <h1 className="mb-4 text-3xl font-bold">{key} - {title}</h1>
       <iframe
         src={iframeSrc}
         style={{ border: 0, width: '100%', height: '405px' }}
