b05524a9e34db8c50dee08461ba842e64078f88c
Dilshod <dilshod.normurodov1392@gmail.com>
2025-06-10 12:16:01 +0900

    added tariffs page

---
+++ app/tariffs/[id]/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TariffDetailPage from './TariffDetailPage'
+
+const page = () => {
+  return (
+    <div className="flex flex-col min-h-screen bg-[#030613] text-white px-6 md:px-20 py-12">
+      <TariffDetailPage />
+    </div>
+  )
+}
+
+export default page
