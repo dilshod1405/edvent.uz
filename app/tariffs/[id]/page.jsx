27d52508e890963601c9ff22acb4f0e8fdb7576e
Dilshod <dilshod.normurodov1392@gmail.com>
2025-06-10 12:19:23 +0900

    added tariffs page

--- app/tariffs/[id]/page.jsx
+++ app/tariffs/[id]/page.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import TariffDetailPage from './TariffDetailPage'
+import Header from '@/components/ui/header'
+import Footer from '@/components/ui/footer'
 
 const page = () => {
   return (
     <div className="flex flex-col min-h-screen bg-[#030613] text-white px-6 md:px-20 py-12">
+      <Header />  
       <TariffDetailPage />
+      <Footer />
     </div>
   )
 }
